a3f91a3e176f78b0c0b410da60e946165a792422
ItepDj <itep.darojat@inmagine.com>
2015-04-07 11:28:42 +0700

    removeing menus

--- headermenu.php
+++
@@ -1,8 +0,0 @@
-
-
-<ul class="nav">
-    <li class=""><a href="./">Home</a></li>
-    <li class="active"><a href="./profile.php">Profile</a></li>
-    <li class=""><a href="./product.php" >Product</a></li>
-    <li class=""><a href="./contact.php">Contact</a></li>
-</ul>

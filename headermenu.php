f393ac539c92d131baff5cb1c6429907f194356a
ItepDj <itep.darojat@inmagine.com>
2015-04-07 11:19:09 +0700

    adding menu for index

---
+++ headermenu.php
@@ -0,0 +1,8 @@
+
+
+<ul class="nav">
+    <li class=""><a href="./">Home</a></li>
+    <li class="active"><a href="./profile.php">Profile</a></li>
+    <li class=""><a href="./product.php" >Product</a></li>
+    <li class=""><a href="./contact.php">Contact</a></li>
+</ul>
